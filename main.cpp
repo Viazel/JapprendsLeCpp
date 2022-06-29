484de3f7f3684d33dbe8fd3e4556ff8a0312f682
Viazel <timeo.morsilli@gmail.com>
2022-06-29 18:16:33 +0200

    Class Entity to learn string and char*

--- main.cpp
+++ main.cpp
@@ -3,36 +3,62 @@
 
 class Entity {
 private:
-    int* i;
-
+    char* m_Name;
+    char* m_Pseudo;
+    unsigned int m_Size;
+    unsigned int m_Size_Pseudo;
 public:
 
-   Entity(const int& i) {
-       this->i = new int(i);
-   }
+    explicit Entity(const char* name) {
+        m_Size = strlen(name) + 1;
+        m_Size_Pseudo = sizeof(char) * 7 + 1;
+        m_Name = new char[m_Size];
+        m_Pseudo = new char[m_Size_Pseudo];
+        memcpy(m_Name, name, m_Size);
+        memcpy(m_Pseudo, "Nothing", m_Size_Pseudo);
+    }
 
-   Entity(const Entity& entity) {
-       i = new int(entity.GetInt());
-   }
+    Entity(const Entity& entity) : m_Size(entity.m_Size), m_Size_Pseudo(entity.m_Size_Pseudo){
+        m_Name = new char[m_Size];
+        memcpy(m_Name, entity.m_Name, m_Size);
+        m_Pseudo = new char[m_Size_Pseudo];
+        memcpy(m_Pseudo, entity.m_Pseudo, m_Size_Pseudo);
+    }
 
-   const int& GetInt() const {
-       return *i;
-   }
+    ~Entity() {
+        delete[] m_Pseudo;
+        delete[] m_Name;
+    }
 
-   void SetInt(const int& index) {
-       *i = index;
-   }
+    const char* GetName() const {return m_Name;}
 
-   ~Entity() {
-       delete i;
-   }
+    void SetName(const char* string) {
+        m_Size = strlen(string) + 1;
+        m_Name = new char[m_Size];
+        memcpy(m_Name, string, m_Size);
+    }
+
+    void SetPseudo(const char* string) {
+        m_Size_Pseudo = strlen(string) + 1;
+        m_Pseudo = new char[m_Size_Pseudo];
+        memcpy(m_Pseudo, string, m_Size_Pseudo);
+    }
+
+    const char* GetPseudo() const {return m_Pseudo;}
+
 };
 
 int main(){
 
-    std::string aa = "Salut";
+    Entity e("Salu");
 
-    std::cout << aa.length() << std::endl;
+    e.SetName("PPPPPPP");
+
+    Entity p = e;
+
+    p.SetPseudo("PPP");
+
+    std::cout << p.GetPseudo() << std::endl;
 
     return 0;
 }
