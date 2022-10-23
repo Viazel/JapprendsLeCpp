0c1b83e0836eaa51fcd2dfcbadcfc43110c2846e
Viazel <timeo.morsilli@gmail.com>
2022-10-23 11:42:29 +0200

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,8 @@
 #include <iostream>
 
-#include "Table/Table.h"
-
 int main() {
 
-	Table ta(49);
-
-	ta.PrintTable();
-
-	std::cout << ta.GetTable(8) << std::endl;
+	std::cout << "Salut" << std::endl;
 
 	return 0;
 }
